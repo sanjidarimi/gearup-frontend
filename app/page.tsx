5a6747c8372847de9163a28b468b99431d39d195
sanjidarimi <sanjidarimi312@gmail.com>
2026-08-01 21:12:02 +0600

    feat:add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/navbar/navbar";
+import { Hero } from "@/components/ui/hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero/>
     </>
   );
 }
